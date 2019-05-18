69a362b4d597e441a796d52606a9dabfb51260db
Richard Session <info@richardsession.com>
2019-05-18 15:43:54 -0400

    Begin adding test suite for ProductRuleMarkdownStrategy

--- __tests__/unit/Product.spec.js
+++ __tests__/unit/Product.spec.js
@@ -1,15 +1,23 @@
 'use strict';
 
 import Product from '../../src/Product';
+import ProductRuleMarkdownStrategy from '../../src/ProductRuleMarkdownStrategy';
+import ProductRuleMarkdownPercentageStrategy from '../../src/ProductRuleMarkdownPercentageStrategy';
+import ProductRuleQuantitySpecialStrategy from '../../src/ProductRuleQuantitySpecialStrategy';
+import ProductRuleBogoSpecialStrategy from '../../src/ProductRuleBogoSpecialStrategy';
 
 describe('Product class', () => {
+    let product;
+
     beforeEach(() => {
-        
+        product = new Product('bananas', 0.69);
     });
 
     test('can retrieve the correct id of the product', () => {
-        const product = new Product('bananas', 0.69);
-
         expect(product.id).toEqual('bananas');
+    });
+
+    test('can retrieve the correct price of the product', () => {
+        expect(product.price).toEqual(0.69);
     });
 });
---
+++ __tests__/unit/ProductRuleMarkdownStrategy.spec.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import ProductRuleMarkdownStrategy from '../../src/ProductRuleMarkdownStrategy';
+
+describe('ProductRuleMarkdownStrategy class', () => {
+    test('markdown price is set correctly', () => {
+        const strategy = new ProductRuleMarkdownStrategy(0.35);
+
+        expect(strategy.markdown).toEqual(0.35);
+    });
+
+    test('markdown price checker throws an error when the price is less than 0', () => {
+        const strategy = new ProductRuleMarkdownStrategy(0.35);
+
+        expect(() => {
+            strategy.checkMarkdown(-0.35);
+        }).toThrowError();
+    });
+
+    test('throws error when the markdown price is less than 0', () => {
+        expect(() => {
+            new ProductRuleMarkdownStrategy(-0.35);
+        }).toThrowError();
+    });
+
+    test('can set markdown price after object construction', () => {
+        const strategy = new ProductRuleMarkdownStrategy(0.5);
+
+        expect(strategy.markdown).toEqual(0.5);
+
+        strategy.markdown = 0.75;
+
+        expect(strategy.markdown).toEqual(0.75);
+    });
+
+    test('throws error when setting markdown price to negative number after object construction', () => {
+        const strategy = new ProductRuleMarkdownStrategy(0.5);
+
+        expect(strategy.markdown).toEqual(0.5);
+
+        expect(() => {
+            strategy.markdown = -0.75;
+        }).toThrowError();
+
+        expect(strategy.markdown).toEqual(0.5);
+    });
+});
