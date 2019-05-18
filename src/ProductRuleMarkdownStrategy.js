151fa17e1c6d3a7842eb74b607f2886b090e53c3
Richard Session <info@richardsession.com>
2019-05-18 15:44:23 -0400

    Add ProductRuleMarkdownStrategy class

---
+++ src/ProductRuleMarkdownStrategy.js
@@ -0,0 +1,36 @@
+'use strict';
+
+export default class ProductRuleMarkdownStrategy
+{
+    constructor (markdownPrice) {
+        this.checkMarkdown(markdownPrice);
+
+        this.markdownPrice = markdownPrice;
+    }
+
+    get markdown () {
+        return this.markdownPrice;
+    }
+
+    set markdown (markdownPrice) {
+        this.checkMarkdown(markdownPrice);
+
+        this.markdownPrice = markdownPrice;
+    }
+
+    apply (lineItem) {
+        const finalPrice = lineItem.product.price - this.markdown;
+
+        if(finalPrice < 0) {
+            throw new Error(lineItem.product.id + ' markdown cannot be higher than the product\'s price');
+        }
+
+        return finalPrice;
+    }
+
+    checkMarkdown (markdownPrice) {
+        if(markdownPrice < 0) {
+            throw new Error('Markdown price cannot be less than 0');
+        }
+    }
+}
