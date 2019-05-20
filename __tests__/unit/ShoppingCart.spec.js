b46baace7f4fc87dc3f77a0454520b91ef3f6c1a
Richard Session <info@richardsession.com>
2019-05-20 15:38:28 -0400

    Test the consolidation of line items withint the cart

--- __tests__/unit/ShoppingCart.spec.js
+++ __tests__/unit/ShoppingCart.spec.js
@@ -41,4 +41,27 @@ describe('ShoppingCart class', () => {
         expect(cart.cart).toHaveLength(1);
         expect(cart.cart[0]).toEqual(lineItem2);
     });
+
+    test('consolidate line items within the cart', () => {
+        const product = new Product('almonds', 7.99);
+        const product2 = new Product('grapes', 2.39);
+        const product3 = new Product('butter', 3.99);
+        const lineItem = new ShoppingCartLineItem(product, 1);
+        const lineItem2 = new ShoppingCartLineItem(product2, 4.32);
+        const lineItem3 = new ShoppingCartLineItem(product3, 2);
+        const lineItem4 = new ShoppingCartLineItem(product2, 2.1);
+        const cart = new ShoppingCart();
+
+        cart.add(lineItem);
+        cart.add(lineItem2);
+        cart.add(lineItem3);
+        cart.add(lineItem4);
+
+        expect(cart.cart).toHaveLength(4);
+
+        const consolidateItems = cart.consolidateLineItems();
+
+        expect(Object.keys(consolidateItems)).toHaveLength(3);
+        expect(consolidateItems['grapes'].quantity).toEqual(6.42);
+    });
 });
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -23,6 +23,37 @@ export default class ShoppingCart
 	 * @returns void
 	 */
 	remove (idx) {
+		if(!this.cart[idx]) {
+			throw new Error('Unable to locate the item within the shopping cart's);
+		}
+
 		this.cart.splice(idx, 1);
+	}
+
+	/**
+	 * Consolidates the line items in the cart in order to aggregate the quantities of repeat items with the associated product.
+	 * Also allows multiple products of the same id but added to the cart at different times to be removed from the cart
+	 * instead of decreasing its overall quantity.
+	 * 
+	 * @returns array
+	 */
+	consolidateLineItems () {
+		const items = [];
+
+		for(let i = 0; i < this.cart.length; i++) {
+			if(!items[this.cart[i].product.id]) {
+				items[this.cart[i].product.id] = {
+					quantity: this.cart[i].quantity,
+					product: this.cart[i].product,
+				};
+
+				continue;
+			}
+
+			// Increment the quantity if product already existed
+			items[this.cart[i].product.id].quantity += this.cart[i].quantity;
+		}
+
+		return items;
 	}
 }
