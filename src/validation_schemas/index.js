587fb67679bd9a90026cd4499c720ed277ca0da9
Richard Session <info@richardsession.com>
2019-05-22 12:04:21 -0400

    Add additional validation schemas

--- src/validation_schemas/index.js
+++ src/validation_schemas/index.js
@@ -3,3 +3,7 @@ import { registerSchema } from 'class-validator';
 registerSchema(require('./Product.json'));
 registerSchema(require('./ShoppingCartLineItem.json'));
 registerSchema(require('./ProductRuleBogoComparisonSpecialStrategy.json'));
+registerSchema(require('./ProductRuleBogoSpecialStrategy.json'));
+registerSchema(require('./ProductRuleQuantitySpecialStrategy.json'));
+registerSchema(require('./ProductRuleMarkdownPercentageStrategy.json'));
+registerSchema(require('./ProductRuleMarkdownStrategy.json'));
